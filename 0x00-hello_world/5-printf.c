a18f05e5f9ed117bc563e2f237db30e1eac0636b
Phumezza <scwebuphumeza@gmail.com>
2023-10-06 15:43:28 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Deacription: printf("with proper grammar, but the outcome is a piece of art,)
-*
-* Return: always 0 (which is success)
- */
+ * with proper grammar, but the outcome is a piece of art
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
