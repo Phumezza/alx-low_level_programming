f4a7ac751a150f81e67c36432c0fd2f72101e861
Phumezza <scwebuphumeza@gmail.com>
2023-10-18 12:31:12 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints all single digit between 0 and 10 in base 10 as char
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+
+		putchar(digit + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
